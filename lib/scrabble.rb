18545c707f48e649bb04bf08d460dec9a47e23e6
Adrian Camacho <adriancamacho18@gmail.com>
2021-07-13 10:21:26 -0700

    add second passing test
    Co-authored-by: Arthur Lee <meleearthur@gmail.com>

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,12 +1,15 @@
-class Scrabble 
+class  Scrabble 
   def initialize(word)
-    @word = word
+    @word = word.split("")
+
   end
 
   def scrabble_score
     score = 0
-    if @word.include?('a')
-      score += 1
+    @word.length.times() do
+      if @word.include?('a')
+        score += 1
+      end
     end
     return score
   end
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -2,9 +2,14 @@ require('pry')
 require('rspec')
 require('scrabble')
 
-describe('#scrabble_score') do
+describe('String#scrabble_score') do
   it("returns a scrabble score for a letter") do
     scrabble1 = Scrabble.new('a')
     expect(scrabble1.scrabble_score()).to(eq(1))
+  end
+
+  it("returns a scrabble score for multiple letters") do
+    scrabble2 = Scrabble.new('aaaa')
+    expect(scrabble2.scrabble_score()).to(eq(4))
   end
 end
