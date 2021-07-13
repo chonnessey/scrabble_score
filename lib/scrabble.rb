c82514160249677f4423be208e9994da872716eb
Adrian Camacho <adriancamacho18@gmail.com>
2021-07-13 11:22:14 -0700

    add third passing test
    Co-authored-by: Arthur Lee <meleearthur@gmail.com>

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,19 +1,19 @@
 class  Scrabble 
   def initialize(word)
     @word = word.split("")
-
+    
   end
-
+  
   def scrabble_score
+    scrabble_values = {"A" => 1, "E" => 1, "I" => 1, "O" => 1, "U" => 1, "L" => 1, "N" => 1, "R" => 1, "S" => 1, "T" => 1, "D" => 2, "G" => 2, "B" => 3, "C" => 3, "M" => 3, "P" => 3, "F" => 4, "H" => 4, "V" => 4, "W" => 4, "Y" => 4, "K" => 5, "J" => 8, "X" => 8, "Q" => 10, "Z" => 10}
     score = 0
-    @word.length.times() do
-      if @word.include?('a')
-        score += 1
-      end
+    @word.each do |key|
+      score += scrabble_values.fetch(key)
     end
     return score
   end
 end
+      
 
 
 
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -4,12 +4,17 @@ require('scrabble')
 
 describe('String#scrabble_score') do
   it("returns a scrabble score for a letter") do
-    scrabble1 = Scrabble.new('a')
+    scrabble1 = Scrabble.new('A')
     expect(scrabble1.scrabble_score()).to(eq(1))
   end
 
   it("returns a scrabble score for multiple letters") do
-    scrabble2 = Scrabble.new('aaaa')
+    scrabble2 = Scrabble.new('AAAA')
     expect(scrabble2.scrabble_score()).to(eq(4))
+  end
+
+  it("returns a scrabble score for multiple different letters") do
+    scrabble3 = Scrabble.new('ZZZZ')
+    expect(scrabble3.scrabble_score()).to(eq(40))
   end
 end
